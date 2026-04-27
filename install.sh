d51fe0025bcc2a9a8b84eb3ab8f9090b1f63bbc7
plauto <141839297+pwrpl3@users.noreply.github.com>
2026-04-27 12:32:18 +0300

    Refactor USB lock/unlock setup in install.sh
    Removed the creation of usb_locker.sh and its associated udev rule. Updated echo statements for clarity.

--- install.sh
+++ install.sh
@@ -5,17 +5,10 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-echo "Setting up USB lock/unlock mechanism..."
+echo "Setting up USB unlock mechanism..."
 
 echo "Removing old USB-related udev rules..."
 rm -f /etc/udev/rules.d/*usb*.rules
-
-echo "Creating /usr/local/bin/usb_locker.sh..."
-cat << 'EOF' > /usr/local/bin/usb_locker.sh
-#!/bin/bash
-/usr/bin/pkill -9 -f kilit
-sudo -u etap /usr/local/bin/i-kilit
-EOF
 
 echo "Creating /usr/local/bin/usb_killer.sh..."
 cat << 'EOF' > /usr/local/bin/usb_killer.sh
@@ -23,17 +16,11 @@ cat << 'EOF' > /usr/local/bin/usb_killer.sh
 /usr/bin/pkill -9 -f kilit
 EOF
 
-chmod +x /usr/local/bin/usb_locker.sh
 chmod +x /usr/local/bin/usb_killer.sh
 
 echo "Creating /etc/udev/rules.d/99-usb-kill.rules..."
 cat << 'EOF' > /etc/udev/rules.d/99-usb-kill.rules
 ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="058f", ATTRS{idProduct}=="6387", RUN+="/usr/local/bin/usb_killer.sh"
-EOF
-
-echo "Creating /etc/udev/rules.d/99-usb-auth.rules..."
-cat << 'EOF' > /etc/udev/rules.d/99-usb-auth.rules
-ACTION=="remove", SUBSYSTEM=="usb", ENV{PRODUCT}=="58f/6387/100", RUN+="/usr/local/bin/usb_locker.sh"
 EOF
 
 echo "Reloading udev rules..."
